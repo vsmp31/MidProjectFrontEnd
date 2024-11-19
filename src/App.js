76c372aa1719773464efb3618f2beb040c31aa27
AvriliaKawonal14 <S22210355@student.unklab.ac.id>
2024-11-20 00:53:00 +0800

    first-commit

---
+++ src/App.js
@@ -0,0 +1,141 @@
+function App() {
+  return (
+    <div>
+      <header className="ds-header">
+        <div className="container text-center">
+          <div className="ds-logo">
+            <a href="index.html">Joana Mertz</a>
+          </div>
+        </div>
+      </header>
+      {/* main */}
+      <main className="ds-pge-wrap">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-sm-12  col-md-6  col-lg-5  col-xl-5  col-xxl-5 ds-left-section">
+              <div className="ds-sticky">
+                <figure>
+                  <img src="images/joana.png" />
+                </figure>
+                <div className="ds-socail">
+                  <span>Contact</span>
+                  <ul>
+                    <li>
+                      <a href="mailto:test@test.com">Email</a>
+                    </li>
+                    <li>
+                      <a href="#" target="_blanck">
+                        Twitter
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" target="_blanck">
+                        Linkedin
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-sm-12  col-md-6  col-lg-7  col-xl-7  col-xxl-7 ds-right-section">
+              <div className="ds-info-sectio-wrp">
+                <section className="ds-info-sectio">
+                  <h2 className="ds-heading">
+                    Freelance for interior designer. Based in Sydney, Australia.
+                  </h2>
+                  <p>
+                    Phasellus non faucibus purus, a venenatis diam. In faucibus
+                    orci mauris, porttitor egestas diam elementum non.
+                    Vestibulum non metus tempus, sagittis libero sed, euismod
+                    purus. Cras dolor nulla, hendrerit vitae libero eu, finibus
+                    ultricies dolor. Vestibulum non metus tempus, sagittis
+                    libero sed, euismod purus. Cras dolor nulla, hendrerit vitae
+                    libero eu, finibus ultricies dolor.
+                  </p>
+                  <p>
+                    Curabitur quis tortor sit amet ligula lobortis auctor. Class
+                    aptent taciti sociosqu ad litora torquent per conubia
+                    nostra, per inceptos himenaeos. Integer ut turpis eget urna
+                    egestas maximus.{" "}
+                  </p>
+                </section>
+                <section className="ds-info-list-section">
+                  <h3 className>What I do</h3>
+                  <ul>
+                    <li>Residential Design</li>
+                    <li>Commercial Design</li>
+                    <li>Interior Design</li>
+                    <li>2D &amp; 3D Design Tools</li>
+                    <li>Design Drafting</li>
+                    <li>Lighting for Interiors</li>
+                    <li>Architectural Illustration</li>
+                    <li>Contracts &amp; Budgeting</li>
+                  </ul>
+                </section>
+                <section className="ds-info-list-section">
+                  <h3 className>Project I have done</h3>
+                  <ul>
+                    <li>
+                      <a href="details.html">Central City Apartment</a>
+                    </li>
+                    <li>
+                      <a href="details.html">Iro-design solutions</a>
+                    </li>
+                    <li>
+                      <a href="details.html">Maroon Beach Hotel</a>
+                    </li>
+                    <li>
+                      <a href="details.html">Innovation Clinic</a>
+                    </li>
+                    <li>
+                      <a href="details.html">Byliner Home Office</a>
+                    </li>
+                    <li>
+                      <a href="details.html">Byte cafe</a>
+                    </li>
+                    <li>
+                      <a href="details.html">Pega</a>
+                    </li>
+                    <li>
+                      <a href="details.html">The Shedquarters</a>
+                    </li>
+                    <li>
+                      <a href="details.html">Royal Hotel</a>
+                    </li>
+                    <li>
+                      <a href="details.html">Humpi restaurant</a>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      {/* main */}
+      {/* footer */}
+      <footer className="ds-footer">
+        <div className="container">
+          <span className="ds-copyright">
+            © 2021 Vanesa Syalom Mamangkey{" "}
+            <a href="https://wwww.designstub.com/" target="_blanck"></a>
+          </span>
+          <a href="https://wwww.designstub.com/" target="_blanck"></a>
+        </div>
+        <a href="https://wwww.designstub.com/" target="_blanck"></a>
+      </footer>
+      <a href="https://wwww.designstub.com/" target="_blanck">
+        {/* footer */}
+        {/* Optional JavaScript; choose one of the two! */}
+        {/* Option 1: Bootstrap Bundle with Popper */}
+        {/* Option 2: Separate Popper and Bootstrap JS */}
+        {/*
+    
+    
+    */}
+      </a>
+    </div>
+  );
+}
+
+export default App;
